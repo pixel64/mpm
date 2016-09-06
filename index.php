0e72c097c7ca31f6baaf7c8b5bb8be9bdc8136b9
pixel64 <matthias.bergmann@iem.thm.de>
2016-09-06 21:40:52 +0200

    added .js build to index.php

--- index.php
+++ index.php
@@ -7,3 +7,23 @@ foreach($files as $file){
     }
 }
 file_put_contents("general.js",$jsfile);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>$Title$</title>
+</head>
+<body>
+  <div id="dropzone" style="width:300px; height:300px; background-color: black;">
+
+  </div>
+</body>
+</html>
+
+<script type="text/javascript" src="lib/general.js"></script>
+<script type="text/javascript" src="lib/eventHandler.js"></script>
+<script type="text/javascript" src="lib/init.js"></script>
+<script>
+InitDragAndDrop();
+</script>
